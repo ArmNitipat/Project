d71b63f86e44510887070c994c79175147e81965
Triphoom Sartsutthi <137309805+RaDis-oF-Magician@users.noreply.github.com>
2023-09-15 01:00:15 +0700

    09/15/2566_v0.0.3

--- MovieProject/myMovieApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MymovieappConfig(AppConfig):
-    name = 'myMovieApp'
--- MovieProject/myMovieApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from myMovieApp import views
-urlpatterns = [
-    path('',views.index),
-    path('calender',views.calender),
-    path('login',views.login)
-]
